98a6c90adc45b1f13943668b0a68a90b72524978
CamJMend <cam.jmend@gmail.com>
2025-04-29 13:16:38 -0600

    Testing de Banking sin Bonus

---
+++ banking-app/src/test/java/com/project/BankAccountTest.java
@@ -0,0 +1,65 @@
+package com.project;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class BankAccountTest {
+
+    private BankAccount account;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() {
+        System.out.println("Setting up a fresh account before each test method");
+        account = new BankAccount(500.0);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        System.out.println("Cleaning up after test method");
+        account = null;
+    }
+
+    @Test(groups = {"positive-tests"})
+    public void testDeposit() {
+        account.deposit(200.0);
+        Assert.assertEquals(account.getBalance(), 700.0, "Balance should be updated correctly after deposit");
+    }
+
+    @Test(groups = {"positive-tests"})
+    public void testWithdraw() {
+        account.withdraw(100.0);
+        Assert.assertEquals(account.getBalance(), 400.0, "Balance should be updated correctly after withdrawal");
+    }
+
+    @Test(groups = {"negative-tests"}, expectedExceptions = IllegalArgumentException.class)
+    public void testNegativeDeposit() {
+        account.deposit(-50.0);
+    }
+
+    @Test(groups = {"negative-tests"}, expectedExceptions = IllegalArgumentException.class)
+    public void testNegativeWithdrawal() {
+        account.withdraw(-30.0);
+    }
+
+    @Test(groups = {"dependency-tests"})
+    public void loginTest() {
+        System.out.println("Logging in");
+    }
+
+    @Test(groups = {"dependency-tests"}, dependsOnMethods = "loginTest")
+    public void transferFundsTest() {
+        System.out.println("Transferring funds after login");
+    }
+
+    @Parameters({"initialAmount", "depositAmount"})
+    @Test(groups = {"parameterized-tests"})
+    public void testParameterizedDeposit(
+            @Optional("1000.0") double initialAmount,
+            @Optional("500.0") double depositAmount) {
+
+        BankAccount paramAccount = new BankAccount(initialAmount);
+        paramAccount.deposit(depositAmount);
+        Assert.assertEquals(paramAccount.getBalance(), initialAmount + depositAmount,
+                "Balance should be updated correctly after deposit");
+    }
+}
